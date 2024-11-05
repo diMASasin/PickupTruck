3ab4eba7f1e6b7041f675fb3ed9deead6671b154
diMASasin <35368058+diMASasin@users.noreply.github.com>
2024-11-05 15:41:07 +0500

    Rename file

--- Assets/Scripts/PlayerComponents/GroundChecker.cs
+++ Assets/Scripts/PlayerComponents/GroundChecker.cs
@@ -13,9 +13,6 @@ public class GroundChecker : MonoBehaviour
         Gizmos.color = Color.red;
         Vector3 origin = transform.position + Vector3.up * _collider.height / 2 + Vector3.forward * 0.1f;
         Vector3 direction = -transform.up;
-        
-        // if (_rigidbody.SweepTest(transform.forward, out RaycastHit hitInfo, 10f))
-        //     Gizmos.DrawWireSphere(hitInfo.point + Vector3.up * 0.5f, 0.5f);
 
         if (Physics.SphereCast(origin, _collider.radius, direction, out RaycastHit hitInfo2, 2f))
         {
